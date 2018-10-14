b075daa96f72d18115da1f7180978ef8f828f911
alicerobinavila <robin.avila@charter.com>
2018-10-14 03:18:46 -0500

    style change

--- src/static/components/userChooser.js
+++ src/static/components/userChooser.js
@@ -4,25 +4,30 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 import UserTypes from "./enums/userTypes";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
 
 class UserChooser extends PureComponent {
     handleUserClick = userType => () => this.props.onChooseUser(userType);
 
     render() {
         return (
-            <Grid item container direction="column">
-                <Grid item container justify="center" style={{marginTop: "20px"}}>
-                    <Typography>What are you here for?</Typography>
-                </Grid>
-                <Grid item container justify="center" alignItems="center">
-                    <Grid item style={{paddingRight:"10px"}}>
-                        <Button onClick={this.handleUserClick(UserTypes.TRANSLATOR)} color="primary" size="large" variant="contained">{UserTypes.TRANSLATOR}</Button>
+            <Card container direction="column" style={{height: 200}}>
+                <CardContent justify="center" style={{marginTop: "20px"}}>
+                    <Grid item container justify="center" alignItems="center">
+                        <Typography>What are you here for?</Typography>
                     </Grid>
+                    <Grid item container justify="center" alignItems="center">
+                        <Grid item style={{paddingRight:"10px"}}>
+                            <Button onClick={this.handleUserClick(UserTypes.TRANSLATOR)} color="primary" size="large" variant="contained">{UserTypes.TRANSLATOR}</Button>
+                        </Grid>
                     <Grid item>
                         <Button onClick={this.handleUserClick(UserTypes.USER)} color="primary" size="large" variant="contained">{UserTypes.USER}</Button>
                     </Grid>
                 </Grid>
-            </Grid>
+                </CardContent>
+            </Card>
         );
     }
 }
